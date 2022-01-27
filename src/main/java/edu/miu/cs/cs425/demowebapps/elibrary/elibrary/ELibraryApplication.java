39c2f60f16e3f2ff30443a607f2b43eac25934ae
Tohfa Nusrat <tohfanusrat@gmail.com>
2022-01-27 17:59:38 -0600

    elibrary
    elibrary

---
+++ src/main/java/edu/miu/cs/cs425/demowebapps/elibrary/elibrary/ELibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.demowebapps.elibrary.elibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELibraryApplication.class, args);
+    }
+
+}
